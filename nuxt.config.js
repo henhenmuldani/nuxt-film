817cac82d9eda7130d0d3a227728ac673a4f031a
henhenmuldani <imammuldani@gmail.com>
2022-09-23 08:39:53 +0700

    deploy to netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,9 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  target: 'static',
+  ssr: 'false',
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/default.scss'],
 
@@ -43,4 +46,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  // 404 not found
+  generate: {
+    fallback: true,
+  },
 }
