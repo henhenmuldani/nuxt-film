13150c4ad87b3772ea108b2b1aa72de545d00a01
henhenmuldani <imammuldani@gmail.com>
2022-09-23 08:21:27 +0700

    deployment

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="app">
+    <Nuxt keep-alive />
+    <!-- keep alive dipakai agar ketika back tidak tembak api lagi -->
+  </div>
+</template>
